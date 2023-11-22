9891a1693a3f3381ad0469bc908bdd79a0faa96c
MartinZelenak <93610343+MartinZelenak@users.noreply.github.com>
2023-11-22 18:22:15 +0100

    Replaced DayOfWeek and Week with Day
    Week wouldn't be used much.
    DayOfWeek could be confusing.

--- environment.py
+++ environment.py
@@ -4,13 +4,12 @@ from typing import NamedTuple, Union
 class TimeSlot(NamedTuple):
     Minute: float
     Hour: int
-    DayOfWeek: int
-    Week: int
+    Day: int
     Month: int
     Year: int
 
     def __str__(self):
-        return f'{self.Hour:02}:{self.Minute:02} {(self.DayOfWeek + (self.Week-1)*7):02}.{self.Month:02}.{self.Year:04}'
+        return f'{self.Hour:02}:{int(self.Minute):02} {(self.Day):02}.{self.Month:02}.{self.Year:04}'
 
     def __add__(self, other: 'TimeSlot | float | int'):
         if isinstance(other, float) or isinstance(other, int):
@@ -18,8 +17,7 @@ class TimeSlot(NamedTuple):
         
         minute = self.Minute + other.Minute
         hour = self.Hour + other.Hour
-        day = self.DayOfWeek + other.DayOfWeek
-        week = self.Week + other.Week
+        day = self.Day + other.Day
         month = self.Month + other.Month
         year = self.Year + other.Year
 
@@ -31,19 +29,15 @@ class TimeSlot(NamedTuple):
             hour -= 24
             day += 1
 
-        if day > 7:
-            day -= 7
-            week += 1
-
-        if week > 4:
-            week -= 4
+        if day > 31:
+            day -= 31
             month += 1
 
         if month > 12:
             month -= 12
             year += 1
 
-        return TimeSlot(minute, hour, day, week, month, year)
+        return TimeSlot(minute, hour, day, month, year)
 
     def __sub__(self, other: 'TimeSlot | float | int'):
         if isinstance(other, float) or isinstance(other, int):
@@ -51,8 +45,7 @@ class TimeSlot(NamedTuple):
         
         minute = self.Minute - other.Minute
         hour = self.Hour - other.Hour
-        day = self.DayOfWeek - other.DayOfWeek
-        week = self.Week - other.Week
+        day = self.Day - other.Day
         month = self.Month - other.Month
         year = self.Year - other.Year
 
@@ -65,18 +58,14 @@ class TimeSlot(NamedTuple):
             day -= 1
 
         if day < 1:
-            day += 7
-            week -= 1
-
-        if week < 1:
-            week += 4
+            day += 31
             month -= 1
 
         if month < 1:
             month += 12
             year -= 1
 
-        return TimeSlot(minute, hour, day, week, month, year)
+        return TimeSlot(minute, hour, day, month, year)
 
     def __lt__(self, other: 'TimeSlot | float | int'):
         if isinstance(other, float) or isinstance(other, int):
@@ -94,17 +83,16 @@ class TimeSlot(NamedTuple):
         return self.to_minutes() == other.to_minutes()
 
     def to_minutes(self):
-        return self.Minute + self.Hour * 60 + (self.DayOfWeek - 1) * 24 * 60 + (self.Week - 1) * 7 * 24 * 60 + (self.Month - 1) * 30 * 24 * 60 + self.Year * 365 * 24 * 60
+        return self.Minute + self.Hour * 60 + (self.Day - 1) * 24 * 60 + (self.Month - 1) * 31 * 24 * 60 + self.Year * 12 * 31 * 24 * 60
     
     @staticmethod
     def from_minutes(min: float | int) -> 'TimeSlot':
         return TimeSlot(
-                Minute = int(min % 60),
+                Minute = min % 60,
                 Hour = int((min // 60) % 24), 
-                DayOfWeek = int((min // (60*24)) % 7 + 1), 
-                Week = int((min // (60*24*7)) % 4 + 1), 
-                Month = int((min // (60*24*30)) % 12 + 1), 
-                Year = int(min // (60*24*365))
+                Day = int((min // (60*24)) % 31 + 1), 
+                Month = int((min // (60*24*31)) % 12 + 1), 
+                Year = int(min // (60*24*31*12))
         )	
 
 
@@ -118,4 +106,7 @@ class TimeSlotEnvironment(simpy.Environment):
 
     def is_weekend(self) -> bool:
         return int((self.now // (60*24)) % 7 + 1) > 5
+    
+    def timeoutUntil(self, time: TimeSlot) -> simpy.Event:
+        return self.timeout(time.to_minutes() - self.now)
     
