b396ce7485b6a12a4fce13ebe201f9e6f0162bcd
MartinZelenak <93610343+MartinZelenak@users.noreply.github.com>
2023-11-24 20:35:36 +0100

    Added type hints to homeModel

--- homeModel.py
+++ homeModel.py
@@ -13,7 +13,7 @@ class Room:
         self.temperature = temperature  # Celsius
         self.devices = []
 
-    def add_device(self, device: SmartDevice):
+    def add_device(self, device: SmartDevice) -> None:
         self.devices.append(device)
 
 
@@ -22,18 +22,18 @@ class Home:
         self.env = env
         self.rooms = {}
 
-    def add_room(self, room: Room):
+    def add_room(self, room: Room) -> None:
         if room.name in self.rooms:
             raise ValueError(f"Home already has a room called {room.name}!")
         self.rooms[room.name] = room
 
-    def go_to_room(self, room_name: str):
+    def go_to_room(self, room_name: str) -> Room:
         if room_name not in self.rooms:
             raise ValueError(f"Home does not have a room called {room_name}!")
         # TODO: log location (needs inhabitant name/id)
         return self.rooms[room_name]
     
-    def get_room(self, room_name: str):
+    def get_room(self, room_name: str) -> Room:
         if room_name not in self.rooms:
             raise ValueError(f"Home does not have a room called {room_name}!")
         # Doesn't log location (used for remote control)
